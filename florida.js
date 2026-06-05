cb2568d2609d4a721ed47e5e1fdd71c3f2e220ee
carolineredhills <caroline@redhillsfl.com>
2026-06-05 15:51:53 -0400

    Update florida.js
    wavecrest again

--- florida.js
+++ florida.js
@@ -1243,7 +1243,7 @@ window.JSMaps.maps.florida = {
 		{
 			"name": "Children's Home Society, Wavecrest",
 			"xPos": 670,
-			"yPos": 380.9,
+			"yPos": 390.9,
 			"color": "#EFD78D",
 			"hoverColor": "#183E6D",
 			"selectedColor": "#EFD78D",
